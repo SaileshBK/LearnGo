505c634597b43662998e4864b9abdac6ce467019
SaileshBK <sailesh.bk1@icloud.com>
2025-07-03 23:23:54 -0500

    feat: This is how loops work

---
+++ basics/loops.go
@@ -0,0 +1,9 @@
+package functions
+
+import("fmt")
+
+func LoopingFunction() {
+    for x := 0; x<10; x++ {
+        fmt.Println("number :", x)
+    }
+}
--- main.go
+++ main.go
@@ -7,6 +7,11 @@ import (
 
 
 func main() {
+    fmt.Println("Adding two numbers")
     response := functions.AddTwoNumbers(1,2)
-    fmt.Println("1+2 =", response)     
+    fmt.Println("1+2 =", response)
+    
+    // Loops
+    fmt.Println("\nLooping")
+    functions.LoopingFunction()     
 }

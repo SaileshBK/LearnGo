c6b465432efffdc856124c23a371b56553613e39
SaileshBK <sailesh.bk1@icloud.com>
2025-07-09 23:42:02 -0500

    feat: This is how conditionals works

--- basics/conditionals.go
+++ basics/conditionals.go
@@ -1,1 +1,31 @@
 package basic
+
+import "fmt"
+
+type Options struct {
+    Value int
+    UseSwitch bool
+}
+
+func IfElseSwitch(x Options) {
+    value := x.Value
+    if x.UseSwitch {
+        UseSwitch(value)
+        return
+    }
+
+    if value%2 == 0 {
+        fmt.Printf("%d is an Even.\n", value)
+    } else {
+        fmt.Printf("%d is an Odd.\n", value)
+    }
+}
+
+func UseSwitch(x int) {
+    switch {
+        case x%2 == 0:
+            fmt.Printf("Using Switch, %d is an Even.\n", x)
+        default:
+            fmt.Printf("Using Switch, %d is an Odd.", x)
+    }
+}
--- main.go
+++ main.go
@@ -7,11 +7,18 @@ import (
 
 func main() {
     // Functions
-    fmt.Println("Adding two numbers")
+    fmt.Println("Adding two numbers:")
     response := basic.AddTwoNumbers(1,2)
     fmt.Println("1+2 =", response)
     
     // Loops
-    fmt.Println("\nLooping")
-    basic.LoopingFunction()     
+    fmt.Println("\nLooping:")
+    basic.LoopingFunction()
+    
+    // Conditionals
+    fmt.Println("\nConditionals:")
+    basic.IfElseSwitch(basic.Options{Value: 10, UseSwitch: false})    
+    basic.IfElseSwitch(basic.Options{Value: 3, UseSwitch: false})    
+    basic.IfElseSwitch(basic.Options{Value: 10, UseSwitch: true})    
+    basic.IfElseSwitch(basic.Options{Value: 3, UseSwitch: true})
 }
